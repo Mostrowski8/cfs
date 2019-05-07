f2dea07720eeaab03383931129a2632aea6703cc
Marek Ostrowski <mostrowski@transperfect.com>
2019-05-07 14:46:20 +0200

    beautify2

--- cfs.js
+++ cfs.js
@@ -7,53 +7,57 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-function readsource (){
-    return new Promise ((resolve, reject) => {
-        fs.readFile("default.txt", "utf8", (err, data)=>{
+function readsource() {
+    return new Promise((resolve, reject) => {
+        fs.readFile("default.txt", "utf8", (err, data) => {
             if (err) {
-            console.log(err);
-            reject(error);
+                console.log(err);
+                reject(error);
             }
             if (data) {
-            console.log("default source: ", data);  
-            resolve(data);   
-        }})
-    }) 
+                console.log("default source: ", data);
+                resolve(data);
+            }
+        })
+    })
 };
 
-function newdefault (src){
-    fs.unlink("default.txt", (err)=>{
+function newdefault(src) {
+    fs.unlink("default.txt", (err) => {
         if (err) console.log(err);
-        fs.writeFile("default.txt", src, "utf8", (err)=>{
+        fs.writeFile("default.txt", src, "utf8", (err) => {
             if (err) console.log(err);
         })
     });
 };
 
-function querries (data){rl.question('Retain default source path? y/n ', (aZero)=>{
-    if (aZero==="y") {
-        copyStuff(path.normalize(data));
-    } else {
-        rl.question('Please provide new source path > ', (aOne) => {
-            rl.question('Set this as new default? y/n> ', (aThree) => {
-                if (aThree==="y") {
-                    copyStuff(path.normalize(aOne));
-                    newdefault (aOne)
-                } else {
-                    copyStuff(path.normalize(aOne));
-                }
-                
-            })    
-    })}
-})};
+function querries(data) {
+    rl.question('Retain default source path? y/n ', (aZero) => {
+        if (aZero === "y") {
+            copyStuff(path.normalize(data));
+        } else {
+            rl.question('Please provide new source path > ', (aOne) => {
+                rl.question('Set this as new default? y/n> ', (aThree) => {
+                    if (aThree === "y") {
+                        copyStuff(path.normalize(aOne));
+                        newdefault(aOne)
+                    } else {
+                        copyStuff(path.normalize(aOne));
+                    }
+
+                })
+            })
+        }
+    })
+};
 
 let options = {
     filter: function (path) {
         return path.indexOf('.txt') === -1
     }
-}; 
+};
 
-function copyStuff(givenPath){
+function copyStuff(givenPath) {
     rl.question('Enter project path > ', (aOne) => {
         rl.question('Enter project name > ', (aTwo) => {
             let directory = path.parse(path.normalize(aOne));
@@ -61,19 +65,19 @@ function copyStuff(givenPath){
             let fulldir = path.normalize(path.join(path.format(directory), filename));
             fs.copy(givenPath, fulldir, options, function (err) {
                 if (err) {
-                  console.error(err);
+                    console.error(err);
                 } else {
-                  console.log("success!");
+                    console.log("success!");
                 }
-              });
+            });
             rl.close();
-        }) 
-    });   
+        })
+    });
 };
 
-async function cfs () {
+async function cfs() {
     let data = await readsource();
-     querries(data)
- };
+    querries(data)
+};
 
 cfs();
